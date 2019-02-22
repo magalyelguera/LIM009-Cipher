625ff65724df343ba5043b0f2b04182763ae92b8
magalyelguera <elguera.magaly@gmail.com>
2019-02-22 15:43:19 -0500

    pantallas terminadas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-const myFunction() {
-    let name = document.getElementById("name").value;
-    document.getElementById("bienvenida").innerHTML = "Bienvenida, " + name;
-    
-    
-    document.getElementById("interfaz1").style.display="none"
-    document.getElementById("interfaz2").style.display="block"
-    
-    }
-      
-    function functionA() {
-    document.getElementById("interfaz2").style.display="none";
-    document.getElementById("responde1").style.display="block";
-      }
-      
 
     
