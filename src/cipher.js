234a1483ef50441b481803eadaeb94b08c522dab
magalyelguera <elguera.magaly@gmail.com>
2019-02-28 13:00:16 -0500

    cifrado y descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,31 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+   let nuevomensaje='';
+   for (let i = 0; i < string.length; i++) {
+     if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+       let numberAcci=((string[i].charCodeAt())-65+offset)%26+65;
+       let lettercipher = String.fromCharCode(numberAcci);
+       nuevomensaje=nuevomensaje+lettercipher;
+     } else {
+       nuevomensaje=nuevomensaje+string[i];
+     }
+    }
+return nuevomensaje;
+ },
+
+  decode: (offset,string) => {
+    let nuevomensaje2='';
+    for (let i =0; i< string.length; i++){
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAcci=((string[i].charCodeAt())-90-offset)%26+90;
+        let lettercipher =String.fromCharCode(numberAcci);
+        nuevomensaje2=nuevomensaje2+lettercipher;
+
+   } else {
+
+     nuevomensaje2=nuevomensaje2+string[i];
+   }
   }
-};
+      return nuevomensaje2;
+}
+    };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 
+
+const texto1 = document.getElementById('one');
+const desplazamiento1 = document.getElementById('off');
+const botonCifrar = document.getElementById('cifrar');
+const resultadoCifrado = document.getElementById('resultado1');
+
+const texto2 = document.getElementById('two');
+const desplazamiento2 = document.getElementById('on');
+const botonDescifrar = document.getElementById('descifrar');
+const resultadoDescifrado = document.getElementById('resultado2');
+
+const capturarCifrado =()=>{ 
+    let mensajeescrito = texto1.value;
+    let numero = parseInt(desplazamiento1.value);
+    let nuevomensaje = cipher.encode(numero,mensajeescrito);
+    return resultadoCifrado.value = nuevomensaje;
+
+}
+
+botonCifrar.addEventListener('click',capturarCifrado)
+
+const capturarDescifrado =()=>{
+    let mensajeescrito2 =texto2.value;
+    let numero2 =parseInt(desplazamiento2.value);
+    let nuevomensaje2 =cipher.decode(numero2,mensajeescrito2);
+    return resultadoDescifrado.value = nuevomensaje2;
+
+}
+ botonDescifrar.addEventListener('click',capturarDescifrado)
+
+
+
+
+
     
