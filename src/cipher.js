87a3f704bc055bb9c1ecf5e69f89f8d712b44c45
magalyelguera <elguera.magaly@gmail.com>
2019-03-12 13:11:36 -0500

    test y readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,65 @@
 window.cipher = {
+  encode : (offset, string) => {
+    let nuevoMensaje='';
+    for (let i = 0; i <string.length; i++) {
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+       let numberAcci = ((string[i].charCodeAt())-65+offset)%26+65;
+       let lettercipher = String.fromCharCode(numberAcci);
+        nuevoMensaje=nuevoMensaje+lettercipher;
+      } else {
+        nuevoMensaje+=nuevoMensaje+string[i];
+      }
+     }
+     return nuevoMensaje;
+  },
+  decode : (offset, string)=> {
+    
+    let nuevoMensaje2='';
+    for (let i =0; i<string.length; i++){
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAcci=((string[i].charCodeAt())-90-offset)%26+90;
+        let lettercipher =String.fromCharCode(numberAcci);
+        nuevoMensaje2=nuevoMensaje2+lettercipher;
+
+   } else {
+
+     nuevoMensaje2+=nuevoMensaje2+string[i];
+   }
+  }
+      return nuevoMensaje2;
+  },
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.cipher = {
   encode: (offset, string) => {
    let nuevoMensaje='';
    for (let i = 0; i <string.length; i++) {
@@ -29,3 +90,4 @@ return nuevoMensaje;
       return nuevoMensaje2;
 }
     };
+    */
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-function ir(){
-    let name= document.getElementById("name").value;
-    document.getElementById("bienvenida").innerHTML = "bienvenid@, " + name;
-
-    document.getElementById("todo").style.display="none";
-    document.getElementById("box2").style.display="block";
-}
 const texto1 = document.getElementById('one-mensaje');
 const desplazamiento1 = document.getElementById('off');
 const botonCifrar = document.getElementById('cifrar');
 const resultadoCifrado = document.getElementById('resultado1');
-
 const texto2 = document.getElementById('two');
 const desplazamiento2 = document.getElementById('on');
 const botonDescifrar = document.getElementById('descifrar');
 const resultadoDescifrado = document.getElementById('resultado2');
+
+//Seleccionado el boton del html
+const boton = document.getElementById('boton');
+
+
+const  ir = () => {
+    let name= document.getElementById("name").value;
+    document.getElementById("bienvenida").innerHTML = "bienvenid@, " + name;
+    document.getElementById("todo").style.display="none";
+    document.getElementById("box2").style.display="block";
+}
+
+//Le asocio el evento de click al boton y defino que función se va a ejecutar 
+//cada vez que haga click
+
+boton.addEventListener('click', ir);
+
+
+
+
 
 
 
@@ -30,7 +42,7 @@ botonCifrar.addEventListener('click',capturarCifrado)
 const capturarDescifrado =()=>{
     let mensajeEscrito2 =texto2.value;
     let numero2 =parseInt(desplazamiento2.value);
-    let nuevoMensaje2 =cipher.decode(numero2,mensajeescrito2);
+    let nuevoMensaje2 =cipher.decode(numero2,mensajeEscrito2);
     return resultadoDescifrado.value = nuevoMensaje2;
 
 }
